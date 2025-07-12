c5776d552be7185e95efc25670587d5a1e473f7d
BigLittle <biglittle@gmail.com>
2025-07-12 20:02:36 +0800

    Change cursor color.

--- lua/nvim-p4/client.lua
+++ lua/nvim-p4/client.lua
@@ -39,7 +39,8 @@ function M.select_client(callback)
     end
     local icon = " "
     vim.cmd("highlight! P4ClientIcon guifg=#ffaa00 guibg=#365a98 gui=bold")
-    vim.cmd("highlight! NuiMenuCursor guifg=NONE guibg=#365a98 gui=bold")
+    vim.cmd("highlight! P4ClientName guibg=#365a98 gui=bold")
+    vim.cmd("highlight! NuiMenuCursor guifg=NONE guibg=NONE gui=bold")
 
     local items = {}
     for _, name in ipairs(clients) do
@@ -70,6 +71,7 @@ function M.select_client(callback)
         on_change = function(item, menu)
             vim.api.nvim_buf_clear_namespace(menu.bufnr, -1, 0, -1)
             vim.api.nvim_buf_add_highlight(menu.bufnr, -1, "P4ClientIcon", item.index, 0, 2)
+            vim.api.nvim_buf_add_highlight(menu.bufnr, -1, "P4ClientName", item.index, 2, -1)
             vim.api.nvim_buf_add_highlight(menu.bufnr, -1, "NuiMenuCursor", item.index, 0, -1)
         end,
 
