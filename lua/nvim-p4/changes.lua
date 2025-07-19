a26f8efa3366ebcc6bad384dae047a9d441249c2
BigLittle <biglittle@gmail.com>
2025-07-19 23:33:10 +0800

    Change color code.

--- lua/nvim-p4/changes.lua
+++ lua/nvim-p4/changes.lua
@@ -68,7 +68,7 @@ function M.open()
 
     local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
     vim.api.nvim_set_hl(0, "P4ChangesHead", { fg = normal_hl.bg } )
-    vim.api.nvim_set_hl(0, "P4ChangesVariable", { fg = "#00cfff" } )
+    vim.api.nvim_set_hl(0, "P4ChangesVariable", { fg = "#51acc5" } )
 
     local tree = Tree({
         bufnr = M.popup.bufnr,
