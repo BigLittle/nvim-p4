069a46f4e3849a5056f15ede1ee182cbdf66f7df
BigLittle <biglittle@gmail.com>
2025-08-13 22:06:02 +0800

    Use global namespace

--- lua/nvim-p4/config.lua
+++ lua/nvim-p4/config.lua
@@ -31,18 +31,18 @@ local defalut = {
 }
 
 local M = {}
-M.ns_id = vim.api.nvim_create_namespace("nvim-p4")
 
 function M.setup(user_opts)
     M.opts = vim.tbl_deep_extend("force", defalut, user_opts or {})
+    M.ns_id = vim.api.nvim_create_namespace("nvim-p4")
 
     local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
-    vim.api.nvim_set_hl(M.ns_id, "P4BlameLine", { fg = "#585858", bg = normal_hl.bg, bold = true })
-    vim.api.nvim_set_hl(M.ns_id, "P4ChangesHead", { fg = normal_hl.bg })
-    vim.api.nvim_set_hl(M.ns_id, "P4ChangesEdit", { fg = "#74c1fc" })
-    vim.api.nvim_set_hl(M.ns_id, "P4ClientHead", { fg = normal_hl.bg })
-    vim.api.nvim_set_hl(M.ns_id, "P4ClientName", { bg = "#365a98", bold = true })
-    vim.api.nvim_set_hl(M.ns_id, "P4ClientIcon", { fg = "#ffaa00", bg = "#365a98", bold = true })
+    vim.api.nvim_set_hl(0, "P4BlameLine", { fg = "#585858", bg = normal_hl.bg, bold = true })
+    vim.api.nvim_set_hl(0, "P4ChangesHead", { fg = normal_hl.bg })
+    vim.api.nvim_set_hl(0, "P4ChangesEdit", { fg = "#74c1fc" })
+    vim.api.nvim_set_hl(0, "P4ClientHead", { fg = normal_hl.bg })
+    vim.api.nvim_set_hl(0, "P4ClientName", { bg = "#365a98", bold = true })
+    vim.api.nvim_set_hl(0, "P4ClientIcon", { fg = "#ffaa00", bg = "#365a98", bold = true })
 
     local utils = require("nvim-p4.utils")
     local client = require("nvim-p4.client")
